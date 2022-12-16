c031741ad75f323fccb34dd02ecf921961ff1ea0
Flame1331 <118307201+Flame1331@users.noreply.github.com>
2022-12-16 13:48:21 -0800

    Update
    added door and stand almost finished gun changed layout.

--- My Assets/Scripts/Projectile.cs
+++ My Assets/Scripts/Projectile.cs
@@ -7,12 +7,44 @@ public class Projectile : MonoBehaviour
     //variables
     public float launchForce = 10f; // Move speed
 
+    //Game manager
+    public GameManager gamemanager;
+
+    //rigidbody
+    private Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
     {
+        gamemanager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * launchForce, ForceMode.Impulse);
+        body = GetComponent<Rigidbody>();
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Range1"))
+        {
+            gamemanager.updateScore1(Random.Range(100, 150));
+            StartCoroutine(destroytime());
+        }
+        if (collision.gameObject.CompareTag("Range2"))
+        {
+            gamemanager.updateScore2(Random.Range(100, 150));
+            StartCoroutine(destroytime());
+        }
+        if (collision.gameObject.CompareTag("Room Parts"))
+        {
+            body.AddForce(Vector3.forward * 0, ForceMode.Impulse);
+            StartCoroutine(destroytime());
+        }
+    }
+
+    IEnumerator destroytime()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
     }
 
 }
