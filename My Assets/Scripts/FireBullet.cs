ecbc9e72e672280c08e74430f451a16822661bc9
Flame1331 <118307201+Flame1331@users.noreply.github.com>
2022-12-16 13:00:45 -0800

    update
    almost finished pistol rework

--- My Assets/Scripts/FireBullet.cs
+++ My Assets/Scripts/FireBullet.cs
@@ -4,22 +4,59 @@ using UnityEngine;
 
 public class FireBullet : MonoBehaviour
 {
+    //Game Manager
+    public GameManager gamemanager;
+
     //game objects
     public GameObject bullet;
     public GameObject bullet_Case;
     public Transform bullet_exit;
     public Transform case_exit;
+    public GameObject Slide;
 
+    //Variables
+    public bool magIn = true;
+    public int ammo = 10;
+    public int recoil = -50;
+
+    void Start()
+    {
+        gamemanager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+
+    public void MagIN()
+    {
+        magIn = true;
+        if(ammo <= 0)
+        {
+            ammo = 10;
+        }
+        gamemanager.updateAmmo(ammo);
+    }
+    public void MagOut()
+    {
+        magIn = false;
+    }
 
 
     public void Shoot()
     {
-        //spawn bullet
-        Instantiate(bullet, bullet_exit.position, bullet_exit.rotation);
+        if (magIn && ammo > 0)
+        {
 
-        //spawn empty bullet case
-        Instantiate(bullet_Case, case_exit.position, case_exit.rotation);
+            //spawn bullet
+            Instantiate(bullet, bullet_exit.position, bullet_exit.rotation);
 
+            //spawn empty bullet case
+            Instantiate(bullet_Case, case_exit.position, case_exit.rotation);
+
+            //Push slide back
+            Slide.transform.Translate(Vector3.forward * recoil * Time.deltaTime);
+
+            ammo -= 1;
+            gamemanager.updateAmmo(ammo);
+        }
     }
 
 
--- My Assets/Scripts/GameManager.cs
+++ My Assets/Scripts/GameManager.cs
@@ -1,25 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //Text
-    public GameObject Score1;
-    public GameObject Score2;
+    //Tablet Text
+    public TextMeshProUGUI Score1;
+    public TextMeshProUGUI Score2;
+    public TextMeshProUGUI Points;
+
+    //Pistol Text
+    public TextMeshProUGUI pistolAmmo;
+
+    //variables
+    private float Scores1;
+    private float Scores2;
+    private float tPoints;
 
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void updateScore1(float score1)
     {
-        
+        Score1.text = "1: " + score1;
+        Scores1 = score1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void updateScore2(float score2)
     {
-        
+        Score2.text = "2: " + score2;
+        Scores2 = score2;
     }
+
+    private void Update()
+    {
+        tPoints = Scores1 + Scores2;
+    }
+
+    public void updatePoints(float points)
+    {
+        Points.text = "Points: " + tPoints;
+    }
+
+    public void updateAmmo(int ammo)
+    {
+        pistolAmmo.text = "Ammo: " + ammo + "/10";
+    }
+
+
 }
